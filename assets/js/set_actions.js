e5bdfa634872cf954955a1a04b433f47745cdb82
Pablo San Gregorio <pabgre@pop-os.localdomain>
2020-04-07 19:23:56 +0200

    click on image implemented

---
+++ assets/js/set_actions.js
@@ -0,0 +1,9 @@
+let action2 = document.getElementById("accion2_btn")
+
+console.log(action2.id)
+
+action2.onclick = function(){
+    console.log("Has pulsado action2")
+    l()
+    //Funcion que quieres
+}
