5f7f4a06434787068eb86af5cee15099333a4eed
rlozano42 <ramonlozano9941@gmail.com>
2020-04-08 20:58:10 +0200

    Web

--- assets/js/set_actions.js
+++ assets/js/set_actions.js
@@ -3,11 +3,9 @@ let action2 = document.getElementById("accion2_btn")
 // console.log(action2.id)
 
 action2.onclick = function() {
-<div>
-    <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
-    <script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"caf4c358e8dfa7e5341b7ca31","lid":"1edc59e762","uniqueMethods":true}) })</script>
-</div>
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+<script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"caf4c358e8dfa7e5341b7ca31","lid":"1edc59e762","uniqueMethods":true}) })</script>
 //    console.log("Has pulsado action2")
 //    l()
-    //Funcion que quieres
+//    Funcion que quieres
 }
