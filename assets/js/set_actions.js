fa142003f03c5d302e0705d6f2005dd704ee7980
rlozano42 <ramonlozano9941@gmail.com>
2020-04-09 13:34:42 +0200

    Proyecto

--- assets/js/set_actions.js
+++ assets/js/set_actions.js
@@ -7,7 +7,4 @@ action2.onclick = function() {
     <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
     <script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"caf4c358e8dfa7e5341b7ca31","lid":"1edc59e762","uniqueMethods":true}) })</script>
 </div>
-//    console.log("Has pulsado action2")
-//    l()
-//    Funcion que quieres
 }
