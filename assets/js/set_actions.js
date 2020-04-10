954c8464086896a9254eff00dd077e62acf1c3a0
rlozano42 <ramonlozano9941@gmail.com>
2020-04-10 17:33:17 +0200

    Web

--- assets/js/set_actions.js
+++ assets/js/set_actions.js
@@ -4,9 +4,7 @@ var action2 = document.getElementById("accion2_btn")
 
 action2.onclick = popup()
 
-function popup() {
+function popup() { 
     var chimp = document.createElement('div')
-    chimp.innerHTML = "<div><script type='text/javascript' src='//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js' data-dojo-config='usePlainJson: true, isDebug: false'></script><script type='text/javascript'>window.dojoRequire(['mojo/signup-forms/Loader'], function(L) { L.start({'baseUrl':'mc.us19.list-manage.com','uuid':'caf4c358e8dfa7e5341b7ca31','lid':'1edc59e762','uniqueMethods':true}) })</script></div>"
-    console.log(action2.id);
-    
+    chimp.innerHTML = "<script type='text/javascript' src='//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js' data-dojo-config='usePlainJson: true, isDebug: false'></script><script type='text/javascript'>window.dojoRequire(['mojo/signup-forms/Loader'], function(L) { L.start({'baseUrl':'mc.us19.list-manage.com','uuid':'caf4c358e8dfa7e5341b7ca31','lid':'1edc59e762','uniqueMethods':true}) })</script>"
 }
